0ab1c9d1cffdfd7f64e60e506ab6c36404063a4b
Al0cam <benjaminsikac@gmail.com>
2025-03-19 19:38:18 +0100

    fixed formatting and use of createEl instead of createSpan, createDiv and new Setting

--- Settings/SettingsTab.ts
+++ Settings/SettingsTab.ts
@@ -3,195 +3,176 @@ import { MovingRule } from "Models/MovingRule";
 import * as obsidian from "obsidian";
 
 export class SettingsTab extends obsidian.PluginSettingTab {
-	plugin: AutoMoverPlugin;
+  plugin: AutoMoverPlugin;
 
-	constructor(app: obsidian.App, plugin: AutoMoverPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: obsidian.App, plugin: AutoMoverPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const { containerEl } = this;
-		containerEl.empty();
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
 
-		containerEl.createEl("h1", { text: "AutoMover settings" });
-		containerEl.createSpan({
-			text: "This plugin will move files to a specified folder based on a regex match.",
-		});
+    new obsidian.Setting(containerEl).setName("Automatic moving").setHeading();
+    new obsidian.Setting(containerEl)
+      .setName("Move on open")
+      .setDesc("Should the file be moved when it is opened?")
+      .addToggle((cb) =>
+        cb.setValue(this.plugin.settings.moveOnOpen).onChange(async (value) => {
+          this.plugin.settings.moveOnOpen = value;
+          await this.plugin.saveData(this.plugin.settings);
+        }),
+      );
 
-		containerEl.createEl("h2", { text: "Automatic moving" });
-		new obsidian.Setting(containerEl)
-			.setName("Move on open")
-			.setDesc("Should the file be moved when it is opened?")
-			.addToggle((cb) =>
-				cb.setValue(this.plugin.settings.moveOnOpen).onChange(async (value) => {
-					this.plugin.settings.moveOnOpen = value;
-					await this.plugin.saveData(this.plugin.settings);
-				}),
-			);
+    new obsidian.Setting(containerEl)
+      .setName("Manually move")
+      .setDesc(
+        "Execute the command to go through your notes and move them according to the rules specified below.",
+      )
+      .addButton((button) => {
+        button.setButtonText("Move files");
+        button.onClick(async () => {
+          this.plugin.goThroughAllFiles();
+        });
+      });
 
-		new obsidian.Setting(containerEl)
-			.setName("Manually move")
-			.setDesc(
-				"Execute the command to go through your notes and move them according to the rules specified below.",
-			)
-			.addButton((button) => {
-				button.setButtonText("Move files");
-				button.onClick(async () => {
-					this.plugin.goThroughAllFiles();
-				});
-			});
+    // there is no default event to move on save, therefore, i'd need to define a new one
+    // running move on change could be an option, but it would produce an overhead in performance because of constant checking for changes
+    //
+    // new obsidian.Setting(containerEl)
+    //   .setName("Move on save")
+    //   .setDesc("Should the file be moved when it is saved?")
+    //   .addToggle((cb) =>
+    //     cb.setValue(this.plugin.settings.moveOnSave).onChange(async (value) => {
+    //       this.plugin.settings.moveOnSave = value;
+    //       await this.plugin.saveData(this.plugin.settings);
+    //     }),
+    //   );
 
-		// there is no default event to move on save, therefore, i'd need to define a new one
-		// running move on change could be an option, but it would produce an overhead in performance because of constant checking for changes
-		//
-		// new obsidian.Setting(containerEl)
-		//   .setName("Move on save")
-		//   .setDesc("Should the file be moved when it is saved?")
-		//   .addToggle((cb) =>
-		//     cb.setValue(this.plugin.settings.moveOnSave).onChange(async (value) => {
-		//       this.plugin.settings.moveOnSave = value;
-		//       await this.plugin.saveData(this.plugin.settings);
-		//     }),
-		//   );
+    /**
+     * Instead of using the default .setting-item class I created a custom class to add other styling,
+     * with the inclusion of the relevant styles from the .setting-item class
+     */
+    const tutorialContainer = containerEl.createDiv({
+      cls: "moving_rules_container",
+    });
+    const headerSetting = new obsidian.Setting(tutorialContainer)
+      .setName("Tutorial")
+      .setHeading();
+    /**
+     * Rule description and tutorial
+     */
+    const description = tutorialContainer.createDiv();
 
-		// TODO: add explanation for the user on how to use stuff and what it does
-		// TODO: explain that they can use regex groups to move files to different folders where they can use regex groups in the folder names
-		// TODO: fix css so that inputs and titles are aligned
+    description.createSpan({
+      text: "This plugin allows you to move files to a specified folder based on the file name.",
+    });
+    description.createEl("br", {});
+    description.createSpan({
+      text: "If you have multiple rules referencing the same file, the first rule will be applied.",
+    });
+    description.createEl("br", {});
+    description.createSpan({
+      text: "Usage of regex groups is supported. To use them in the folder name, use $1, $2, etc. to reference the group.",
+    });
 
-		/**
-		 * Instead of using the default .setting-item class I created a custom class to add other styling,
-		 * with the inclusion of the relevant styles from the .setting-item class
-		 */
-		const tutorialContainer = containerEl.createEl("div", {
-			cls: "moving_rules_container",
-		});
-		const header = tutorialContainer.createEl("div", { cls: "rule_header" });
-		header.createEl("h2", { text: "Tutorial" });
-		/**
-		 * Rule description and tutorial
-		 */
-		const description = tutorialContainer.createEl("div");
+    /**
+     * Examples
+     */
+    description.createEl("br", {});
+    description.createEl("br", {});
+    description.createSpan({ text: "Examples: ", cls: "rule_title" });
 
-		description.createEl("span", {
-			text: "You can use regex to match the file name and move it to a specified folder.",
-		});
-		description.createEl("br", {});
-		description.createSpan({
-			text: "If you have multiple rules referencing the same file, the first rule will be applied.",
-		});
-		description.createEl("br", {});
-		description.createSpan({
-			text: "	Usage of regex groups is supported. To use them in the folder name, use $1, $2, etc. to reference the group.",
-		});
+    description.createEl("br", {});
+    description.createSpan({
+      text: "To move files containing 'Scroll' to a folder named 'Scrolls': ",
+    });
+    const example1 = description.createDiv({ cls: "rule margig_right" });
+    example1.createSpan({ text: "Scroll", cls: "rule_title" });
+    example1.createSpan({ text: "Scrolls", cls: "rule_title" });
 
-		/**
-		 * Examples
-		 */
-		description.createEl("br", {});
-		description.createEl("br", {});
-		description.createEl("span", {
-			text: "Examples: ",
-			cls: "rule_title",
-		});
+    description.createSpan({
+      text: "To move files containing the word 'Scroll' and a number to space -> to a folder named 'Scrolls' under the subfolder of the scroll number: ",
+    });
+    const example2 = description.createDiv({ cls: "rule margig_right" });
+    example2.createSpan({ text: "Scroll (\\d+)", cls: "rule_title" });
+    example2.createSpan({ text: "Scrolls\/$1", cls: "rule_title" });
 
-		description.createEl("br", {});
-		description.createSpan({
-			text: "To move files containing 'Scroll' to a folder named 'Scrolls': ",
-		});
-		const example1 = description.createDiv({ cls: "rule margig_right" });
-		example1.createEl("span", {
-			text: "Scroll",
-			cls: "rule_title",
-		});
-		example1.createEl("span", {
-			text: "Scrolls",
-			cls: "rule_title",
-		});
+    /**
+     * Header of the rules
+     */
+    const movingRulesContainer = containerEl.createDiv({
+      cls: "moving_rules_container",
+    });
 
-		description.createSpan({
-			text: "To move files containing the word 'Scroll' and a number divided to space -> to a folder named 'Scrolls' under the subfolder of the scroll number: ",
-		});
-		const example2 = description.createDiv({ cls: "rule margig_right" });
-		example2.createEl("span", {
-			text: "Scroll (\\d+)",
-			cls: "rule_title",
-		});
-		example2.createEl("span", {
-			text: "Scrolls\\$1",
-			cls: "rule_title",
-		});
+    new obsidian.Setting(movingRulesContainer)
+      .setName("Moving rules")
+      .setHeading();
 
-		/**
-		 * Header of the rules
-		 */
-		const movingRulesContainer = containerEl.createEl("div", {
-			cls: "moving_rules_container",
-		});
-		movingRulesContainer.createEl("h2", { text: "Regex moving rules", cls: "rule_header" });
+    const ruleList = movingRulesContainer.createDiv({ cls: "rule_list" });
+    const ruleHeader = ruleList.createDiv({ cls: "rule margig_right" });
+    ruleHeader.createEl("p", {
+      text: "Search criteria (string or regex)",
+      cls: "rule_title",
+    });
+    ruleHeader.createEl("p", {
+      text: "Folder (string that can contain regex groups)",
+      cls: "rule_title",
+    });
 
-		const ruleList = movingRulesContainer.createEl("div", { cls: "rule_list" });
-		const ruleHeader = ruleList.createEl("div", { cls: "rule margig_right" });
-		ruleHeader.createEl("p", {
-			text: "Search Criteria (String or Regex)",
-			cls: "rule_title",
-		});
-		ruleHeader.createEl("p", {
-			text: "Folder (String that can contain Regex groups)",
-			cls: "rule_title",
-		});
+    const addButton = ruleHeader.createEl("button", {
+      text: "+",
+      cls: "rule_button",
+    });
+    addButton.addEventListener("click", () => {
+      this.plugin.settings.movingRules.push(new MovingRule());
+      // this is used to rerender the settings tab
+      this.display();
+    });
 
-		const addButton = ruleHeader.createEl("button", {
-			text: "+",
-			cls: "rule_button",
-		});
-		addButton.addEventListener("click", () => {
-			this.plugin.settings.movingRules.push(new MovingRule());
-			// this is used to rerender the settings tab
-			this.display();
-		});
-		/**
-		 * List of rules
-		 */
-		for (const rule of this.plugin.settings.movingRules) {
-			const child = ruleList.createEl("div", { cls: "rule" });
-			child.createEl("input", {
-				value: rule.regex,
-				cls: "rule_input",
-			}).onchange = (e) => {
-				rule.regex = (e.target as HTMLInputElement).value;
-				this.plugin.settings.movingRules.map((r) => (r === rule ? rule : r));
-				this.plugin.saveData(this.plugin.settings);
-			};
-			child.createEl("input", {
-				value: rule.folder,
-				cls: "rule_input",
-			}).onchange = (e) => {
-				rule.folder = (e.target as HTMLInputElement).value;
-				this.plugin.settings.movingRules.map((r) => (r === rule ? rule : r));
-				this.plugin.saveData(this.plugin.settings);
-			};
+    /**
+     * List of rules
+     */
+    for (const rule of this.plugin.settings.movingRules) {
+      const child = ruleList.createDiv({ cls: "rule" });
+      child.createEl("input", {
+        value: rule.regex,
+        cls: "rule_input",
+      }).onchange = (e) => {
+        rule.regex = (e.target as HTMLInputElement).value;
+        this.plugin.settings.movingRules.map((r) => (r === rule ? rule : r));
+        this.plugin.saveData(this.plugin.settings);
+      };
+      child.createEl("input", {
+        value: rule.folder,
+        cls: "rule_input",
+      }).onchange = (e) => {
+        rule.folder = (e.target as HTMLInputElement).value;
+        this.plugin.settings.movingRules.map((r) => (r === rule ? rule : r));
+        this.plugin.saveData(this.plugin.settings);
+      };
 
-			const duplicateButton = child.createEl("button", {
-				text: "⿻",
-				cls: "rule_button rule_button_duplicate",
-			});
-			duplicateButton.addEventListener("click", () => {
-				this.plugin.settings.movingRules.push(
-					new MovingRule(rule.regex, rule.folder),
-				);
-				this.display();
-			});
+      const duplicateButton = child.createEl("button", {
+        text: "⿻",
+        cls: "rule_button rule_button_duplicate",
+      });
+      duplicateButton.addEventListener("click", () => {
+        this.plugin.settings.movingRules.push(
+          new MovingRule(rule.regex, rule.folder),
+        );
+        this.display();
+      });
 
-			const deleteButton = child.createEl("button", {
-				text: "x",
-				cls: "rule_button rule_button_remove",
-			});
-			deleteButton.addEventListener("click", () => {
-				this.plugin.settings.movingRules =
-					this.plugin.settings.movingRules.filter((r) => r !== rule);
-				this.display();
-			});
-		}
-	}
+      const deleteButton = child.createEl("button", {
+        text: "x",
+        cls: "rule_button rule_button_remove",
+      });
+      deleteButton.addEventListener("click", () => {
+        this.plugin.settings.movingRules =
+          this.plugin.settings.movingRules.filter((r) => r !== rule);
+        this.display();
+      });
+    }
+  }
 }
