d615af51e1135e20d0aedff592afbaafd99f4544
Al0cam <benjaminsikac@gmail.com>
2025-04-13 11:12:30 +0200

    Formatting

--- Models/MovingRule.ts
+++ Models/MovingRule.ts
@@ -1,16 +1,16 @@
 export class MovingRule {
-	/**
-	 * This filed is used for defining the regex with its group matchers
-	 */
-	public regex: string;
-	/**
-	 * This field is used for defining the folder to which the files will be moved
-	 * It can use the previously defined groups to create folders with the same name
-	 */
-	public folder: string;
+  /**
+   * This filed is used for defining the regex with its group matchers
+   */
+  public regex: string;
+  /**
+   * This field is used for defining the folder to which the files will be moved
+   * It can use the previously defined groups to create folders with the same name
+   */
+  public folder: string;
 
-	constructor(regex?: string, folder?: string) {
-		this.regex = regex || "";
-		this.folder = folder || "";
-	}
+  constructor(regex?: string, folder?: string) {
+    this.regex = regex || "";
+    this.folder = folder || "";
+  }
 }
